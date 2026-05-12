8ed2b290da3ee8fa2d12d1fde1badd016bc52849
MrDeck <mrdeckgamedev@gmail.com>
2026-05-12 11:35:38 +0200

    añadido funcion para meter estudios a la lista

--- portafolio/script.js
+++ portafolio/script.js
@@ -1,8 +1,15 @@
-let theme = "light";
 const botonTema = document.getElementsByClassName('tema')[0];
+const botonEstudios = document.getElementsByClassName('btn-estudio')[0];
 const header = document.getElementById('header');
 const footer = document.getElementById('footer');
+const estudioLista = document.getElementById('estudioLista');
 const storage = window.localStorage;
+let theme = "light";
+let estudios = [
+    {"anio": 2020, "nombre": "Educación secundaria obligatorias", "institucion": "IES Vicente Cano"},
+    {"anio": 2020, "nombre": "Grado medio de sistemas microinformaticos y redes", "institucion": "IES Juan Bosco"},
+    {"anio": 2020, "nombre": "Grado superior de desarrollo de aplicaciones multiplataforma (en proceso)", "institucion": "IES Juan Bosco"}
+]
 
 botonTema.onclick = function () {
     if (botonTema.textContent == "Claro") {
@@ -53,3 +60,57 @@ function chargeTheme() {
     theme = storage.getItem("theme");
     changeTheme();
 }
+
+function onLoadFunction() {
+    chargeTheme();
+    chargeEstudios();
+}
+
+function chargeEstudios() {
+    /*
+    if (storage.getItem("estudios") != null) {
+        estudios = storage.getItem("estudios");
+    }
+    */
+    for(let i in estudios) {
+        estudioLista.appendChild(createEstudio(estudios[i]["anio"], estudios[i]["nombre"], estudios[i]["institucion"]));
+    }
+}
+
+function createEstudio(anioN, nombreN, institucionN) {
+  const tarjeta = document.createElement('li');
+  tarjeta.textContent = anioN + " - " + nombreN + " - " + institucionN;
+  return tarjeta;
+}
+
+botonEstudios.onclick = function () {
+    // Source - https://stackoverflow.com/a/29949852
+    // Posted by Jeff Daze, modified by community. See post 'Timeline' for change history
+    // Retrieved 2026-05-12, License - CC BY-SA 4.0
+    var formData = new FormData(document.getElementById('formularioEstudio'));
+    if (!formData.get("anio")) {
+        return;
+    }
+    if (!formData.get("nombre")) {
+        return;
+    }
+    if (!formData.get("institucion")) {
+        return;
+    }
+
+    estudios.push(
+        {
+            "anio": formData.get("anio"), 
+            "nombre": formData.get("nombre"), 
+            "institucion": formData.get("institucion")
+        }
+    );
+    estudioLista.appendChild(
+        createEstudio(
+            formData.get("anio"),
+            formData.get("nombre"),
+            formData.get("institucion")
+        )
+    )
+    //storage.setItem("estudios", estudios);
+};
